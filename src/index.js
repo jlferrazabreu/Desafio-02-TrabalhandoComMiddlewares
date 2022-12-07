fcd5a0ad9d511295163ff50f86b9805a5ca863be
Jorge Luis Ferraz de Abreu <jlferrazabreu@gmail.com>
2022-12-06 22:17:34 -0300

    feat: index.js Implementando os middleware conforme solicitado no desafio 02

--- src/index.js
+++ src/index.js
@@ -10,19 +10,74 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'Username already exists' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const user = request.user;
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({ error: 'Free plan, already have 10 todos registered' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: 'Username already exists' });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  const regexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+
+  const validateId = regexExp.test(id);
+
+  if (!validateId) {
+    return response.status(400).json({ error: 'todo.id is not in the correct format' });
+  }
+
+  if (!todo) {
+    return response.status(404).json({ error: 'todo already exists' });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User already exists' });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
